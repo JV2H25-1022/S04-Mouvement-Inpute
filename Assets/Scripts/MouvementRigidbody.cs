843b9dfaa88b1ee1afb1709b45b53c196ba5daea
Jean-Michele <morin.emilemorin@gmail.com>
2025-02-14 15:50:16 -0500

    a
    a

--- Assets/Scripts/MouvementRigidbody.cs
+++ Assets/Scripts/MouvementRigidbody.cs
@@ -8,7 +8,7 @@ public class MouvementRigidbody : MonoBehaviour
     
     void Start()
     {
-        // Prends une réference au Rigidbody attaché a cet objet.
+        // Prends une rï¿½ference au Rigidbody attachï¿½ a cet objet.
         _rb = GetComponent<Rigidbody>();
     }
 
@@ -17,6 +17,6 @@ public class MouvementRigidbody : MonoBehaviour
         // Vector3 deplacement = _rb.position + new Vector3(1f * Time.fixedDeltaTime, 0, 0);
         // _rb.MovePosition(deplacement);
 
-        // _rb.velocity = transform.TransformDirection(Vector3.forward);
+        _rb.velocity = transform.TransformDirection(Vector3.forward);
     }
 }
